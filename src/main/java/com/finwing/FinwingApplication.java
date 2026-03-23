37ebf7245d6466e56f4eef2212160c23b16d6a6c
yoarpit <arpitjayshwal707@gmail.com>
2026-03-23 22:20:54 +0530

    Initial commit - Finwing Spring Boot project

---
+++ src/main/java/com/finwing/FinwingApplication.java
@@ -0,0 +1,12 @@
+package com.finwing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinwingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinwingApplication.class, args);
+    }
+}
---
+++ src/main/java/com/finwing/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.finwing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("appName", "Finwing");
+        model.addAttribute("message", "Welcome to Finwing ");
+        return "index"; // loads index.html
+    }
+}
+
