0ea51226681582ef7bd4c3e8cbb4b5f1c8e070e9
yoarpit <arpitjayshwal707@gmail.com>
2026-04-13 21:07:59 +0530

    deploy

--- finwing/src/test/java/com/finwing/FinwingApplicationTests.java
+++ finwing/src/test/java/com/finwing/FinwingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.finwing;
+// package com.finwing;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FinwingApplicationTests {
+// @SpringBootTest
+// class FinwingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
